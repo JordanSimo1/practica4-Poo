71521db950ad0e55194596f2af6a8bbe501ffdf2
JordanSimo1 <144269807+JordanSimo1@users.noreply.github.com>
2023-11-06 01:21:04 -0800

    se agrego para la posicion del metodo grafico

---
+++ Coordenadas.java
@@ -0,0 +1,35 @@
+
+public class Coordenadas {
+    private int posX;
+    private int posY;
+    private char letra;
+
+    public Coordenadas( char letra) {
+        this.letra = letra;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public char getLetra() {
+        return letra;
+    }
+
+    public void setLetra(char letra) {
+        this.letra = letra;
+    }
+    
+}
